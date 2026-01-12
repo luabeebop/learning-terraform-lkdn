8542cf9c41b927bd22cfcc075c3091ee70c91a59
Yong Cimatu <160300203+luabeebop@users.noreply.github.com>
2026-01-12 03:28:39 +0000

    Deleted outputs as it is not needed and removed unnecessary filters from main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["Amazon Linux 2023 (kernel-6.1)"]
-  }
-}
-
 resource "aws_instance" "EC2" {
   ami           = "ami-01dc51e87421923b6"
   instance_type = "t3.micro"
