8561604ec1bcced2138132743b6c299a8da0bb8f
Cimatu <yonghwan.cimatu@puma.com>
2026-01-14 16:32:17 +0800

    #5:Changed the OS into ubuntu

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "EC2" {
-  ami           = "ami-01dc51e87421923b6"
+  ami           = "ami-00d8fc944fb171e29"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "terrapuma"
+    Name = "terrabuntu"
   }
 }
